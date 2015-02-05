d3bc4517ca43149ce821ce349b53c45849835fb7
Kyle Martens <martensk42@gmail.com>
2015-02-05 13:18:25 -0800

    commit animation

---
+++ Animation.js
@@ -0,0 +1,47 @@
+function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, lineSize, startline, scale, loop, reverse, reverseOffset) {
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameHeight = frameHeight;
+    this.frameDuration = frameDuration;
+    this.frames = frames;
+    this.lineSize = lineSize;
+    this.startline = startline;
+    this.scale = scale;
+    this.loop = loop;
+    this.reverse = reverse;
+    this.reverseOffset = reverseOffset;
+
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    else {
+        var frame = this.currentFrame();
+        var xindex = frame % this.lineSize;
+        var yindex = Math.floor(frame / this.lineSize) + this.startline;
+
+        if(reverse)
+            xindex += reverseOffset;
+
+        ctx.drawImage(this.spriteSheet,
+                     xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+                     this.frameWidth, this.frameHeight,
+                     x, y,
+                     this.frameWidth * this.scale,
+                     this.frameHeight * this.scale);
+    }
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
