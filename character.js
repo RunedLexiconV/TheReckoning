d445de6982304eaed56ecfc7400b3a6a596d9ca1
Joshua Moore <joshua.j.moore@gmail.com>
2015-02-07 00:40:29 -0800

    idk

--- character.js
+++ character.js
@@ -3,10 +3,10 @@ var FRAME_HEIGHT = 215;
 var FRAME_DURATION = .1;
 
 
-function Character (spritesheet, portrait) { 
+function Stickman(spritesheet, portrait) { 
 	this.spritesheet = spritesheet;
     this.portrait = portrait;
-	that = this;
+	var that = this;
 // The animation parameters are as follows:
 
 // spriteSheet, int frameWidth, int frameHeight,
@@ -14,40 +14,38 @@ function Character (spritesheet, portrait) {
 // int scale, bool loop, bool reverse, int reverseOffset
 
     this.animations = {
-    	idle: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 10, 5, 0,
-    						1.5, true, false, 0),
-    	walk: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 5, 5, 2,
-    						1.5, true, false, 0),
-    	punch1: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 7, 5, 3,
-    						1.5, false, false, 0),
-    	punch2: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 5, 5, 5,
-    						1.5, false, false, 0),
-    	punch3: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 8, 5, 6,
-    						1.5, false, false, 0),
-    	kick1: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 8, 5, 8,
-    						1.5, false, false, 0),
-    	kick2: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 6, 5, 10,
-    						1.5, false, false, 0),
-    	kick3: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 9, 5, 12,
-    						1.5, false, false, 0),
-    	jump: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 5, 5, 14,
-    						1.5, false, false, 0),
-    	inair: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 5, 5, 15,
-    						1.5, false, false, 0),
-    	landing: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
-    						FRAME_DURATION, 5, 5, 16,
-    						1.5, false, false, 0),
+        idle: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 10, 5, 0,
+                            1.5, true, false, 0),
+        walk: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 5, 5, 2,
+                            1.5, true, false, 0),
+        punch1: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 7, 5, 3,
+                            1.5, false, false, 0),
+        punch2: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 5, 5, 5,
+                            1.5, false, false, 0),
+        punch3: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 8, 5, 6,
+                            1.5, false, false, 0),
+        kick1: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 8, 5, 8,
+                            1.5, false, false, 0),
+        kick2: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 6, 5, 10,
+                            1.5, false, false, 0),
+        kick3: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 9, 5, 12,
+                            1.5, false, false, 0),
+        jump: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 5, 5, 14,
+                            1.5, false, false, 0),
+        inair: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 5, 5, 15,
+                            1.5, false, false, 0),
+        landing: new Animation(that.spritesheet, FRAME_WIDTH, FRAME_HEIGHT,
+                            FRAME_DURATION, 5, 5, 16,
+                            1.5, false, false, 0)
     };
 }
-
-//define all available Characters here
--- main.js
+++ main.js
@@ -52,11 +52,11 @@ AM.downloadAll( function () {
 	  	gameEngine.startInput();
       document.getElementById("canvas").focus();
       timer = startBackgroundAnimation(gameEngine, "./sprites/background"+background+"/", 36);
-      var character1 = new Character(AM.getAsset("./sprites/sheet 2a.png"), AM.getAsset("./sprites/portrait1.png"));
+      var character1 = new Stickman(AM.getAsset("./sprites/sheet 2a.png"), AM.getAsset("./sprites/portrait1.png"));
       gameEngine.addEntity(new Player(gameEngine, character1,
                                       50 , GROUND - FRAME_HEIGHT,
                                       HEALTH, PLAYER1_CONTROLS));
-      var character2 = new Character(AM.getAsset("./sprites/sheet 2a.png"), AM.getAsset("./sprites/portrait1.png"));
+      var character2 = new Stickman(AM.getAsset("./sprites/sheet 2a.png"), AM.getAsset("./sprites/portrait1.png"));
       gameEngine.addEntity(new Player(gameEngine, character2,
                                       500 , GROUND - FRAME_HEIGHT,
                                       HEALTH, PLAYER2_CONTROLS));
