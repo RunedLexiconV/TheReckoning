d27de315280fe707d611b5e2e2f227ecbbb6c27d
Kyle Martens <martensk42@gmail.com>
2015-03-04 15:56:12 -0800

    got the AI inheritence working plus fixed the issue with 52 loops per call

--- aiPlayer.js
+++ aiPlayer.js
@@ -1,9 +1,26 @@
 function aiPlayer (game, character, x, y, health) {
-	Player.call(game, character, x, y, health, PLAYER2_CONTROLS);
-	attackRadius = 70;
-	fleeRadius = 100;
+	Player.call(this, game, character, x, y, health, PLAYER2_CONTROLS);
+	aiPlayer.prototype = new Player(game, character, x, y, health, PLAYER2_CONTROLS);
+	aiPlayer.prototype.constructor = aiPlayer;
+	this.attackRadius = 70;
+	this.fleeRadius = 100;
 }
 
-aiPlayer.prototype.chooseMove = function(first_argument) {
-	
+aiPlayer.prototype.chooseMove = function() {
+	var entities = this.game.entities;
+	for (var i = 0; i < entities.length; i++) {
+		if (entities[i] != this) {
+			var otherGuy = entities[i];
+			
+		}
+	}
+
 };
+
+aiPlayer.prototype.update = function() {
+	this.chooseMove();
+	Player.prototype.update.call(this);
+};
+
+aiPlayer.prototype.draw = function() {Player.prototype.draw.call(this);};
+aiPlayer.prototype.handleInput = function() {};
